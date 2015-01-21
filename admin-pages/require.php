5e47705fa7c7c2f05d78ff6bc5eff0bd94043474
Crystal Barton <cbarto11@uncc.edu>
2015-01-21 14:52:04 -0500

    remove unneeded Settings page.

--- admin-pages/require.php
+++ admin-pages/require.php
@@ -7,7 +7,6 @@ require_once( ORGANIZATION_HUB_PLUGIN_PATH.'/classes/model/sites-model.php' );
 require_once( dirname(__FILE__).'/pages/users.php' );
 require_once( dirname(__FILE__).'/pages/upload.php' );
 require_once( dirname(__FILE__).'/pages/sites.php' );
-require_once( dirname(__FILE__).'/pages/settings.php' );
 require_once( dirname(__FILE__).'/pages/log.php' );
 
 require_once( dirname(__FILE__).'/tabs/users/list.php' );
